60d72b5979b600a6ab79456a4ec671e469525c1d
jorgef29 <jorgefigueredo2001@gmail.com>
2024-05-02 22:34:00 -0400

    a

--- src/components/ModalProveedor.jsx
+++ src/components/ModalProveedor.jsx
@@ -1,20 +1,42 @@
-import React from 'react'
-import Button from "react-bootstrap/Button"
-import Modal from "react-bootstrap/Modal"
-import App from "../App"
+import React from "react";
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
+import App from "../App";
+import { FormProveedores } from "./FormProveedores";
 //props para mostrar el modal
-export const ModalProveedor = ({show,handleClose,actualizarProveedores, proveedor, isEdit}) => {
+export const ModalProveedor = ({
+  show,
+  handleClose,
+  actualizarProveedores,
+  proveedor,
+  isEdit,
+}) => {
   return (
     <>
-        <Modal show={show} onHide={handleClose} size="xl">
-            <Modal.Header> 
-                <Modal.Title className='p-1'> <h1>Crear proveedor</h1></Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-                <App proveedor={proveedor} actualizarProveedores={actualizarProveedores} isEdit={isEdit}
-                handleClose={handleClose}/>
-            </Modal.Body>
-        </Modal>
+      <Modal show={show} onHide={handleClose} size="xl">
+        <Modal.Header>
+          <Modal.Title className="p-1">
+            {" "}
+            <h1>Crear proveedor</h1>
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          {/*
+          <App
+            proveedor={proveedor}
+            actualizarProveedores={actualizarProveedores}
+            isEdit={isEdit}
+            handleClose={handleClose}
+          />
+  */}
+          <FormProveedores
+            proveedor={proveedor}
+            actualizarProveedores={actualizarProveedores}
+            isEdit={isEdit}
+            handleClose={handleClose}
+          />
+        </Modal.Body>
+      </Modal>
     </>
-  )
-}
+  );
+};
--- src/main.jsx
+++ src/main.jsx
@@ -1,39 +1,8 @@
-import React, { Fragment } from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap/dist/js/bootstrap.bundle.js'
-import { FormProductos } from './components/FormProductos.jsx'
-import { ListadoProveedores } from './components/ListadoProveedores.jsx'
-import { NavBarMine } from './components/NavBarMine.jsx'
-import { ListadoPedidosCompras } from './components/ListadoPedidosCompras.jsx'
-import ListarDepositos from './components/ListarDepositos.jsx'
-import ListarProducto from './components/ListarProductos.jsx'
-import ListarCategorias from './components/ListarCategorias.jsx'
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.js";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    {/*
-    <FormProductos/>
-     <ListadoProveedores/> 
-     <FormProductos/>
-      <ListadoPedidosCompras/>
-
-      //esto debe ir en app.jsx
-      no hace falta el StrictMode
-*/}
-  <Router>
-    <NavBarMine/>
-          <Routes>
-            <Route path="/proveedores" element={<ListadoProveedores/>} />
-            <Route path="/depositivos" element={<ListarDepositos/>} />
-            <Route path="/categorias" element={<ListarCategorias/>} />
-            <Route path="/productos" element={<ListarProducto/>} />
-          </Routes>
-
-      </Router>
-
-     </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
